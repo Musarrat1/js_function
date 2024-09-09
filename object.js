132f5eb791e6ca801b1e00ecbffaa8a2dcb717af
Musarrat1 <junuarabegum8@gmail.com>
2024-09-09 22:22:09 +0600

    added last one

---
+++ object.js
@@ -0,0 +1,19 @@
+function detail(info){
+  const future = info.age+10;
+  return future;
+
+
+
+
+}
+
+const  person ={
+    name: "musarrat",
+    age :23,
+    location:'bangladesh',
+    work :'student',
+    female : true 
+}
+  const result = detail(person);
+
+console.log("amar age", result ,"hoye gelo");
---
+++ reverse.js
@@ -0,0 +1,8 @@
+function isRev(n){
+    
+    return ;
+
+}
+const num = "musarrat";
+const rev = isRev(num);
+console.log(rev);
